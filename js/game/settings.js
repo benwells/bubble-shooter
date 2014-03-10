ea4112c316fc39547abaf4932bca712e4b466e22
Ben Wells <mrbrwells@gmail.com>
2014-03-09 23:00:12 -0400

    added particles and explosion

--- js/game/settings.js
+++ js/game/settings.js
@@ -16,12 +16,13 @@ function game() {
   this.level = 1;
   this.message = new message("LEVEL " + this.level);
   this.score = new score();
+  this.particles = [];
 
   this.draw = function () {
     var _this = this,
-        context = _this.context,
-        context2 = _this.context2,
-        player = _this.player;
+      context = _this.context,
+      context2 = _this.context2,
+      player = _this.player;
 
     player.draw();
 
@@ -35,6 +36,10 @@ function game() {
 
     for (i = 0; i < _this.explosions.length; i++) {
       _this.explosions[i].draw(context);
+    }
+
+    for (i = 0; i < _this.particles.length; i++) {
+      _this.particles[i].draw(context2);
     }
 
     _this.message.draw(context2);
@@ -89,11 +94,15 @@ function game() {
       explosion.update();
     });
 
+    _this.particles.forEach(function (particle) {
+      particle.update(20);
+    });
+
     _this.message.update();
 
     if (Math.random() < 0.01) { //this number controls the frequency of enemies
       _this.enemies.push(Enemy({
-              x: _this.CANVAS_WIDTH,
+        x: _this.CANVAS_WIDTH,
         cHeight: _this.CANVAS_HEIGHT
       }));
     }
@@ -125,13 +134,13 @@ function game() {
 
   this.detectCollision = function (enemies, bullets, ctxt) {
     var _this = this,
-        xmax,
-        xmin,
-        ymax,
-        ymin,
-        enemyX,
-        enemyY,
-        enemyRadius;
+      xmax,
+      xmin,
+      ymax,
+      ymin,
+      enemyX,
+      enemyY,
+      enemyRadius;
 
     enemies.forEach(function (enemy) {
       enemyX = enemy.x;
@@ -145,6 +154,12 @@ function game() {
       bullets.forEach(function (bullet) {
         if (bullet.x <= xmax && bullet.x >= xmin && bullet.y <= ymax && bullet.y >= ymin) {
           _this.updateScore(enemy.points).explosions.push(new explosion(enemy.explode(ctxt)));
+
+          var x = randomFloat(100, 400);
+          var y = randomFloat(100, 400);
+          _this.createExplosion(enemyX, enemyY, "#525252");
+          _this.createExplosion(enemyX, enemyY, "#FFA318");
+
           bullet.active = false;
         }
       });
@@ -161,4 +176,42 @@ function game() {
     //make chainable
     return this;
   };
+
+  /*
+   * Advanced Explosion effect
+   * Each particle has a different size, move speed and scale speed.
+   *
+   * Parameters:
+   *  x, y - explosion center
+   *  color - particles' color
+   */
+  this.createExplosion = function (x, y, color) {
+    var minSize = 10;
+    var maxSize = 30;
+    var count = 10;
+    var minSpeed = 60.0;
+    var maxSpeed = 200.0;
+    var minScaleSpeed = 1.0;
+    var maxScaleSpeed = 4.0;
+
+    for (var angle = 0; angle < 360; angle += Math.round(360 / count)) {
+      var particle = new Particle();
+
+      particle.x = x;
+      particle.y = y;
+
+      particle.radius = randomFloat(minSize, maxSize);
+
+      particle.color = color;
+
+      particle.scaleSpeed = randomFloat(minScaleSpeed, maxScaleSpeed);
+
+      var speed = randomFloat(minSpeed, maxSpeed);
+
+      particle.velocityX = speed * Math.cos(angle * Math.PI / 180.0);
+      particle.velocityY = speed * Math.sin(angle * Math.PI / 180.0);
+
+      this.particles.push(particle);
+    }
+  }
 }
