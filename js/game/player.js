2214c5fe0e75ee7dc47b1d5163a11afece5bf252
Ben Wells <mrbrwells@gmail.com>
2014-03-11 17:15:13 -0400

    Player now inherits from ObjectBase

--- js/game/player.js
+++ js/game/player.js
@@ -1,26 +1,47 @@
-function player(canvasContext) {
-  this.canvas = canvasContext;
+function Player(opts) {
+  ObjectBase.call(this);
+
+  this.canvas = {};
   this.color = "#00A";
   this.x = 40;
   this.y = 100;
   this.width = 40;
   this.height = 32;
   this.bullets = [];
-  this.midpoint = function () {
-    return {
-      x: this.x + this.width / 2,
-      y: this.y + this.height / 2
-    };
-  };
-  this.draw = function (canvas) {
-    this.canvas.drawImage(document.getElementById('shipImg'),this.x, this.y, this.width, this.height);
-  };
-  this.shoot = function () {
-    var bulletPosition = this.midpoint();
-    this.bullets.push(Bullet({
-      speed: 20,
-      x: bulletPosition.x,
-      y: bulletPosition.y
-    }));
-  };
+
+  this.setOpts(opts);
 }
+//inheritance from ObjectBase Class
+Player.prototype = new ObjectBase();
+Player.prototype.constructor = Player;
+
+//Class Methods
+Player.prototype.draw = function (canvas) {
+  this.canvas.drawImage(document.getElementById('shipImg'),this.x, this.y, this.width, this.height);
+};
+
+Player.prototype.midpoint = function () {
+  return {
+    x: this.x + this.width / 2,
+    y: this.y + this.height / 2
+  };
+};
+
+Player.prototype.shoot = function () {
+  var bulletPosition = this.midpoint();
+  var b = new Bullet({
+    'speed': 20,
+    'x': bulletPosition.x,
+    'y': bulletPosition.y,
+    context: this.canvas
+  });
+
+  this.bullets.push(b);
+
+  // this.bullets.push(new Bullet({
+  //   speed: 20,
+  //   x: bulletPosition.x,
+  //   y: bulletPosition.y,
+  //   context: this.canvas
+  // }));
+};
