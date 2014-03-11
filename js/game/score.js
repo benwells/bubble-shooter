6cc79c9b47530e68dba7aba18edcaa329419fe6c
Ben Wells <mrbrwells@gmail.com>
2014-03-11 14:19:18 -0400

    removed update method from score

--- js/game/score.js
+++ js/game/score.js
@@ -6,14 +6,6 @@ function Score() {
   this.score = 0;
   this.x = 780 - 10;
   this.y = 420;
-
-  // this.draw = function (context2) {
-  //   context2.font = this.fontSize + "px " + this.fontStyle;
-  //   context2.fillStyle = this.color;
-  //   context2.textBaseline = "bottom";
-  //   context2.textAlign="right";
-  //   context2.fillText(this.label + this.score, this.x, this.y);
-  // };
 }
 
 Score.prototype.draw = function (context2) {
